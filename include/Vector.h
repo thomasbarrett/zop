74c2f2438bd5fc81ffcfb37057cc7e4e3ddd844f
Thomas Barrett <tbarrett@caltech.edu>
2020-03-02 09:24:42 +0000

    Added methods to vector class

--- include/Vector.h
+++ include/Vector.h
@@ -2,6 +2,7 @@
 #define ZAP_VECTOR_H
 
 #include <vector>
+#include <functional>
 #include <cmath>
 
 namespace loki {
@@ -43,6 +44,65 @@ public:
         return Vector::divide(*this, norm());
     }
 
+    Vector map(std::function<double(double)> f) const {
+        Vector res(dim());
+        for (int i = 0; i < dim(); i++) {
+            res[i] = f(data_[i]);
+        }
+        return res;
+    }
+
+    Vector reduce(std::function<double(double, double)> f, double initial) const {
+        double acc = initial;
+        for (int i = 0; i < dim(); i++) {
+            acc = f(acc, data_[i]);
+        }
+        return acc;
+    }
+
+    int argmin() const {
+        int imin = 0;
+        double min = data_.front();
+        for (int i = 1; i < dim(); i++) {
+            if (data_[i] < min) {
+                min = data_[i];
+                imin = i;
+            }
+        }
+        return imin;
+    }
+
+    int argmax() const {
+        int imax = 0;
+        double min = data_.front();
+        for (int i = 1; i < dim(); i++) {
+            if (data_[i] > min) {
+                min = data_[i];
+                imax = i;
+            }
+        }
+        return imax;
+    }
+
+    int max() const {
+        return data_[argmax()];
+    }
+
+    int min() const {
+        return data_[argmin()];
+    }
+
+    double sum() const {
+        double acc = 0.0;
+        for (double e: data_) {
+            acc += e;
+        }
+        return acc;
+    }
+
+    double mean() const {
+        return sum() / dim();
+    }
     //=----------------------- Operator Overloads --------------------------=//
 
     double& operator[](size_t i) {
@@ -183,6 +243,16 @@ public:
             a[2] * b[0] - a[0] * b[2],
             a[0] * b[1] - a[1] * b[0]
         }; 
+    }
+
+    friend std::ostream& operator<<(std::ostream &os, const Vector &v) {
+        os << "[";
+        for (int i = 0; i < v.dim(); i++) {
+            if (i != 0) os << ", ";
+            os << v[i];
+        }
+        os << "]";
+        return os;
     }
 };
 
