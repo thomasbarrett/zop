d4681557cd02714caf6cb00e51a1023bff0c99d4
SprintRay 3 <sprintray-3@ESP_4DC468.localdomain>
2020-03-04 14:17:09 -0800

    updated autodoc

--- docs/generate.py
+++ docs/generate.py
@@ -3,6 +3,14 @@
 import clang.cindex
 import glob
 import json
+from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
+import os
+
+def ensure_dir(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 def fully_qualified(c):
     if c is None:
@@ -53,15 +61,45 @@ def find_methods(node, filename):
     
 
 if __name__ == "__main__":
+    clang.cindex.Config.set_library_file('/usr/local/Cellar/llvm/9.0.1/lib/libclang.dylib')
     index = clang.cindex.Index.create()
     for file in glob.glob('./include/*'):
-        classes = {}
-        print(file)
-        translation_unit = index.parse(file, args=['-std=c++17', '-Iinclude', '-I/usr/lib/clang/9.0.1/include', '-x', 'c++'])
+
+        include_path_macos = [
+            '-Iinclude',
+            '-I/Library/Developer/CommandLineTools/usr/include/',
+            '-I/Library/Developer/CommandLineTools/usr/include/c++/v1/',
+            '-I/usr/local/Cellar/llvm/9.0.1/lib/clang/9.0.1/include/'
+        ]
+
+        include_path_linux = [
+            '-Iinclude',
+            '-I/usr/lib/clang/9.0.1/include',
+        ]
+
+        include_path = include_path_macos
+        translation_unit = index.parse(file, args=['-std=c++17', *include_path, '-x', 'c++'])
+
         if (len(list(translation_unit.diagnostics)) > 0):
             for error in list(translation_unit.diagnostics):
                 print(error)
         else:
             filename = translation_unit.spelling
             find_methods(translation_unit.cursor, filename)
-            print(json.dumps(classes))
+
+
+    file_loader = FileSystemLoader('docs/templates')
+    env = Environment(loader=file_loader, autoescape=True)
+
+    links = list(map(lambda class_name: {
+        'spelling': "::".join(class_name.split("::")[1:]),
+        'href': f'{"/".join(class_name.split("::")[1:])}.html'
+    }, classes.keys()))
+
+    for class_name in classes.keys():
+        html_name = f'docs/{"/".join(class_name.split("::"))}.html'
+        ensure_dir(html_name)
+        with open(html_name,'w') as file:
+            template = env.get_template('class.html')
+            output = template.render(class_=classes[class_name], links=links)
+            file.write(output)
